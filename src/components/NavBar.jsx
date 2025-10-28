830b8fcebec52ce22beb3b76dc35da963ce1df4a
John Ralph Dela Rosa <you@example.com>
2025-10-29 05:45:15 +0800

    navbar enhancement

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,32 @@
-import { Link } from "react-router-dom";
-import "../css/NavBar.css"
+import { Link, useLocation } from "react-router-dom";
+import "../css/NavBar.css";
 
-function NavBar(){
-    return <nav className="navbar">
-        <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
-        </div>
-        <div className="nabar-links">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/favorites" className="nav-link">Favorites</Link>
-        </div>
+function NavBar() {
+  const location = useLocation();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">🎬 MovieApp</Link>
+      </div>
+      <div className="navbar-links">
+        <Link
+          to="/"
+          className={`nav-link ${location.pathname === "/" ? "active" : ""}`}
+        >
+          Home
+        </Link>
+        <Link
+          to="/favorites"
+          className={`nav-link ${
+            location.pathname === "/favorites" ? "active" : ""
+          }`}
+        >
+          Favorites
+        </Link>
+      </div>
     </nav>
-};
+  );
+}
 
 export default NavBar;
